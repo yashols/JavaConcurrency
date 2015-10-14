d5d3bc8a446b6d41bf4dd63fe13fe00a19b3724e
yashols <yashol2014@gmail.com>
2015-10-14 21:43:35 +0530

    First solution

--- src/main/java/com/problems/first/Solution1.java
+++ src/main/java/com/problems/first/Solution1.java
@@ -1,10 +1,74 @@
 package com.problems.first;
 
 /**
- * Created by <NAME> on 10/14/2015.
+ * This class creates 4 thread and each thread prints its own number
+ * T1 - 1
+ * T2 - 2
+ * T3 - 3
+ * T4 - 4
+ * T1 - 5
+ * T2 - 6
+ * ETC
+ *
+ * @author Yashol
  */
 public class Solution1 {
+    public static final Object lock = new Object();
 
-    //Solution will go here
+    public static void main(String arg[]) {
+        Counter counter = new Counter(1);
+        Thread t1 = new Thread(new CounterPrinter(0, counter));
+        Thread t2 = new Thread(new CounterPrinter(1, counter));
+        Thread t3 = new Thread(new CounterPrinter(2, counter));
+        Thread t4 = new Thread(new CounterPrinter(3, counter));
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
 
+class CounterPrinter implements Runnable {
+    private final int threadNumber;
+    private volatile Counter counter;
+
+    public CounterPrinter(int threadNumber, Counter counter) {
+        this.threadNumber = threadNumber;
+        this.counter = counter;
+    }
+
+    public void run() {
+        synchronized (Solution1.lock) {
+            while (counter.getCount() <= 20) {
+                try {
+                    if (counter.getCount() % 4 != threadNumber) {
+                        Solution1.lock.wait();
+                    } else {
+                        Thread.sleep(300);
+                        System.out.println("Printed by Thread " + threadNumber + " counter - " + counter.getCount());
+                        counter.setCount(counter.getCount() + 1);
+                        Solution1.lock.notifyAll();
+                    }
+                } catch (Exception ie) {
+                    ie.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class Counter {
+    private volatile int count;
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
