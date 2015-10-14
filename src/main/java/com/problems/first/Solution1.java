a6c0524928f63e60fd3e23a6d1bfe66ffa82081f
yashols <yashol2014@gmail.com>
2015-10-14 10:18:22 +0530

    First problem and sample package for solutions

---
+++ src/main/java/com/problems/first/Solution1.java
@@ -0,0 +1,10 @@
+package com.problems.first;
+
+/**
+ * Created by <NAME> on 10/14/2015.
+ */
+public class Solution1 {
+
+    //Solution will go here
+
+}
---
+++ src/main/java/com/problems/first/Solution2.java
@@ -0,0 +1,10 @@
+package com.problems.first;
+
+/**
+ * Created by <NAME> on 10/14/2015.
+ */
+public class Solution2 {
+
+    //Solution 2 will go here
+
+}
